081ab8977f89f3c006f3768cf823b50948b7d628
Maksym Shchyhol <drayv.mj12@gmail.com>
2023-03-28 23:36:34 +0200

    Local kestrel port changed to match local IIS one

--- aspnet-core/centy/Program.cs
+++ aspnet-core/centy/Program.cs
@@ -1,19 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var httpPortString = Environment.GetEnvironmentVariable("HTTPPORT");
+_ = int.TryParse(httpPortString, out var httpPort);
+
 var portString = Environment.GetEnvironmentVariable("PORT");
 _ = int.TryParse(portString, out var port);
 
+var aspNetEnv = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(port); // to listen for incoming http connection on port 80
-    // options.ListenAnyIP(443, configure => configure.UseHttps()); // to listen for incoming https connection on port 443
+    options.ListenAnyIP(httpPort); // to listen for incoming http connection on port 80
+    if (aspNetEnv == "Development")
+    {
+        options.ListenAnyIP(port, configure => configure.UseHttps()); // to listen for incoming https connection on port 443
+    }
 });
 
 var app = builder.Build();
