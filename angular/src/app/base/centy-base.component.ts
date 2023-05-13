d2025441854537e3f58c0dbda2e30ce1d1cdcc5e
Maksym Shchyhol <drayv.mj12@gmail.com>
2023-05-14 00:14:27 +0200

    sidebar added

--- angular/src/app/base/centy-base.component.ts
+++ angular/src/app/base/centy-base.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-centy',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./centy-base.component.scss', './toolbar.scss']
 })
 export class CentyComponent {
+  mobileQuery: MediaQueryList;
 
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
--- angular/src/material.module.ts
+++ angular/src/material.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
     exports: [
         MatToolbarModule,
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        MatSidenavModule
     ]
 })
 export class MaterialModule { }
