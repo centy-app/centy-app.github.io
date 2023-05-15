d28d2502bbf05949f212caf6db02355f4abc237a
Maksym Shchyhol <drayv.mj12@gmail.com>
2023-05-15 03:47:39 +0200

    Logout button added

--- angular/src/app/base/centy-base.component.ts
+++ angular/src/app/base/centy-base.component.ts
@@ -1,18 +1,23 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-centy',
   templateUrl: './centy-base.component.html',
   styleUrls: ['./centy-base.component.scss', './toolbar.scss']
 })
-export class CentyComponent {
+export class CentyComponent implements OnDestroy {
   @ViewChild('sidenav') public sidenav: MatSidenav;
   public mobileQuery: MediaQueryList;
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(
+    private _router: Router,
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher) {
+
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
@@ -22,6 +27,11 @@ export class CentyComponent {
     if (this.mobileQuery.matches) {
       this.sidenav.close();
     }
+  }
+
+  public onLogoutClick() {
+    // TODO: clear the auth token
+    this._router.navigateByUrl('/');
   }
 
   ngOnDestroy(): void {
