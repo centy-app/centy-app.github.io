23e73e76bbfc628b75e8c8fdbff6e4132cd8ceeb
Maksym Shchyhol <drayv.mj12@gmail.com>
2024-10-24 17:51:41 +0200

    Auto-generated commit

---
+++ chunk-UMAUW5TW.js
@@ -0,0 +1,1 @@
+var r=(()=>{let s=class s{constructor(e){this.payload=e}};s.type="[Auth] Log In";let t=s;return t})(),a=(()=>{let s=class s{};s.type="[Auth] Log Out";let t=s;return t})();export{r as a,a as b};
